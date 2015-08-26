4c98354841d383e6c62d463414a22a1ea6707b24
mxro <mxro@nowhere.com>
2015-08-26 17:24:08 +1000

    Updating Databases API Autocommit Wed Aug 26 17:24:08 EST 2015

--- src/main/java/de/mxro/async/map/sql/SqlConfigurations.java
+++ src/main/java/de/mxro/async/map/sql/SqlConfigurations.java
@@ -2,4 +2,37 @@ package de.mxro.async.map.sql;
 
 public class SqlConfigurations {
 
+    public static SqlConnectionConfiguration inMemoryH2() {
+        return new SqlConnectionConfiguration() {
+            
+            @Override 
+            public String getDriverClassName() {
+                return "org.h2.Driver";
+            }
+
+            @Override 
+            public boolean supportsInsertOrUpdate() {
+                return false;
+            }
+
+            @Override 
+            public boolean supportsMerge() {
+                return true;
+            }
+
+            @Override 
+            public String getMergeTemplate() {
+                return "MERGE INTO "+getTableName()+" (Id, Value) KEY (Id) VALUES (?, ?)";
+            }
+
+            override String getConnectionString() {
+                return "jdbc:h2:mem:test"
+            }
+
+            override String getTableName() {
+                return "test"
+            }
+        };
+    }
+
 }
