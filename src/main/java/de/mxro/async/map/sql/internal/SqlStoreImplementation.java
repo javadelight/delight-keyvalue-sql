d284bf41c51b323d3a972d54977b33c2adadd320
mxro <mxro@nowhere.com>
2016-01-29 13:57:09 +1100

    Developing ObjectHub and improving engine Autocommit Fri Jan 29 13:57:09 EST 2016

--- src/main/java/de/mxro/async/map/sql/internal/SqlStoreImplementation.java
+++ src/main/java/de/mxro/async/map/sql/internal/SqlStoreImplementation.java
@@ -918,7 +918,7 @@ public class SqlStoreImplementation<V> implements StoreImplementation<String, V>
 
         this.writeWorker = new WriteWorker(OneUtilsJre.newJreConcurrency().newExecutor().newSingleThreadExecutor(this),
                 new ConcurrentLinkedQueue<String>());
-        this.writeWorker.setDelay(20);
+        // this.writeWorker.setDelay(20);
 
         this.commitThread = Executors.newFixedThreadPool(1);
 
